dab3e551e7f8db2cfccc06c170c39b39ad6e0a7a
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-13 21:42:11 +0300

    views was developped

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -2,32 +2,44 @@ from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from django.contrib import messages
+from .models import City
 
 def index(request):
-    API_KEY=config("API_KEY")
-    city="Erzurum"
-    url= f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-    response=requests.get(url)
-    content=response.json()
-    # pprint(content)
-    # pprint(content["name"])
-    # pprint(content["main"]["temp"])
-    # pprint(content["weather"][0]["description"])
-    # pprint(content["weather"][0]["icon"])
+    API_KEY = config("API_KEY")
+    city = "Erzurum"
+    u_city = request.POST.get("name")
+    
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        print(response.ok)
+        
+        if response.ok:
+            content = response.json()
+            r_city = content["name"]
+            if City.objects.filter(name=r_city):
+                messages.warning(request, "City already exists!")
+            else:
+                City.objects.create(name=r_city)
+            
+        else:
+            messages.warning(request, "There is no city")
+    
+    
+    
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    content = response.json()
 
-    context={
-        "city": content["name"],
-        "temp": content["main"]["temp"],
-        "icon": content["weather"][0]["icon"],
-        "desc": content["weather"][0]["description"]
 
-
+    context = {
+        "city" : content["name"],
+        "temp" : content["main"]["temp"],
+        "icon" : content["weather"][0]["icon"],
+        "desc" : content["weather"][0]["description"]
     }
-
-
-
-
-
+    
     return render(request, 'weatherapp/index.html', context)
 
 
