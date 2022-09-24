a4194edc3b416eea3829cdefa1dbee5e2f5447d5
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-24 16:56:31 +0300

    adding datetime on th screen

---
+++ weatherapp/static/weatherapp/js/datetime.js
@@ -0,0 +1,9 @@
+
+
+
+let currentDate = new Date();
+let cDay = currentDate.getDate()
+let cMonth = currentDate.getMonth() + 1
+let cYear = currentDate.getFullYear()
+
+time= cDay + "/" + cMonth + "/" + cYear
